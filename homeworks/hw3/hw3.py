2ae027d4e13952ce229e4024ec9f8c1f1a53379c
sina.alparslan <sinaalparslan@gmail.com>
2023-02-05 21:16:18 +0300

    hw3 almost done,readme file created

--- homeworks/hw3/hw3.py
+++ homeworks/hw3/hw3.py
@@ -1,25 +1,63 @@
-import pandas as pd
-def read_file(file:str):
-    """
-    dosyayı okuyup içindeki değerleri elde etme.
-    :param file: file name with format ex. data.csv
+import pandas
+
+
+def calculate_average_value(rolling_average_value: int, df: list):
+    '''
+    values sütunundaki değerleri birer,ikişer,üçer.... şekilde toplayıp ortlamasını alıp yeni sütununda gerekli
+    konumunda yazıyor.
+
+    :param rolling_average_value: int value of rolling average value
+    :param df: pandas dataFrame
+    :return: updated pandas dataFrame
+    '''
+    b = []
+    for i in range(len(df)):
+
+        if i <= rolling_average_value:
+            b.append(None)
+        else:
+            sub_df = df['values'][i - 1 - rolling_average_value: i + 1]
+            sum_df = sum(sub_df)
+            len_df = len(sub_df)
+            res = sum_df / len_df
+            b.append(res)
+
+    df[f"avg_{rolling_average_value + 2}"] = b
+
+
+def rolling_average_value(df: list):
+    '''
+    yeni oluşturacağımız sütun sayısınca işlemleri döndürmek için
+    :param df: pandas dataFrame
     :return:
-    """
-    data_df = pd.read_csv(f'{file}')
+    '''
+    for i in range(100):
+        calculate_average_value(i, df)
+    write_file(df)
 
-    mydt=pd.DataFrame(data_df, columns=(['values']))
 
-    return mydt
+def read_file():
+    '''
+    csv verisini dataFrame şekline okuyabilmek için
+    :return: pandas dataFrame
+    '''
+    values = pandas.read_csv('data.csv')
+    return values
 
-def creat_column(values:'pandas.core.frame.DataFrame'):
-    for i in range(len(values)):
-        values.insert(f'{i+1}values',)
+
+def write_file(df):
+    '''
+    güncellenmiş pandas dataFrame i csv formatında oluşturmak için
+    :param df: pandas dataFrame
+    :return: .csv file
+    '''
+    df.to_csv("result.csv")
 
 
 def main():
-    file = 'data.csv'
-    my_dict = read_file(file)
+    data = read_file()
+
+    rolling_average_value(data)
 
 
-    creat_column(my_dict)
 main()
